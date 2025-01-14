a67051ce0f1ae2a588b0d850d1aa83a2125159bc
Marcos Oliveira <antonio.oliveira@aluno.unievangelica.edu.br>
2025-01-14 20:50:39 -0300

    Revert "Alterando o título mais uma vez"
    This reverts commit 2435d1179c9aa5daf3d35954ec4127e7b318b83f.

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,5 +1,5 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Desafio concluído';
+titulo.innerHTML = 'Desafio aceito';
 
 function verificar() {
     console.log('O botão foi clicado');
