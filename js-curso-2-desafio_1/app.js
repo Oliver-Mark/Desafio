ca1619fc40e7749025d784e2c061886c639c3205
Marcos Oliveira <antonio.oliveira@aluno.unievangelica.edu.br>
2025-01-14 18:59:39 -0300

    Projeto desafio

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,22 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function verificar() {
+    console.log('O botão foi clicado');
+}
+
+function exibir() {
+    alert('Eu amo JS');
+}
+
+function nome() {
+    let cidade = prompt('Informa uma cidade do Brasil:');
+    alert(`Estive em ${cidade} e lembrei de você`);
+}
+
+function calculo() {
+    let numero1 = parseInt(prompt('Inform um número:'));
+    let numero2 = parseInt(prompt('Informe outro número:'));
+    let soma = numero1 + numero2;
+    alert(`A soma de ${numero1} com o ${numero2} é igual a ${soma}`);
+}
